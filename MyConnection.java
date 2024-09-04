96981bc0b35dfa81fefd5051b2f41fff730e9e96
AmanMalviya08 <164153862+AmanMalviya08@users.noreply.github.com>
2024-09-04 08:21:17 +0530

    Initial commit

---
+++ MyConnection.java
@@ -0,0 +1,23 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+
+    private static final String username = "root";
+    private static final String password = "888900";
+    private static final String dataConn = "jdbc:mysql://localhost:3306/student_management";
+    private static Connection con = null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(dataConn, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return con;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Database for Student Database management System
+  Key features of the database is to include search functionality and data validation, maintaining accurate records. This system provides a reliable solution for educational institutions to effectively manage student information, streamline administrative tasks, and ensure secure data handling.
